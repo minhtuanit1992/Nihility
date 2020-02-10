8d51f1c4f0ef52e929bc98cd27e8e38edd66f906
minhtuanit1992 <minhtuanit1992@gmail.com>
2020-02-10 22:55:28 +0700

    Complete

---
+++ Nihility.X0.Solution/Nihility.X0.Solution/Areas/Administrator/Controllers/BaseUIController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nihility.X0.Solution.Areas.Administrator.Controllers
+{
+    public class BaseUIController : Controller
+    {
+        public ActionResult Card()
+        {
+            return View();
+        }
+    }
+}
